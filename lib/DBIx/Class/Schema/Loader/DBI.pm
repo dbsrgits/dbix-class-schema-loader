7507c8ce0703993b79b31bf90144e28ef97d4a9d
Brandon Black <blblack@gmail.com>
2007-06-08 01:11:22 +0000

    update versions for 0.04000 release, added some better feature stuff to Makefile.PL

--- lib/DBIx/Class/Schema/Loader/DBI.pm
+++ lib/DBIx/Class/Schema/Loader/DBI.pm
@@ -7,7 +7,7 @@ use Class::C3;
 use Carp::Clan qw/^DBIx::Class/;
 use UNIVERSAL::require;
 
-our $VERSION = '0.03999_02';
+our $VERSION = '0.04000';
 
 =head1 NAME
 
--- lib/DBIx/Class/Schema/Loader/DBI/Writing.pm
+++ lib/DBIx/Class/Schema/Loader/DBI/Writing.pm
@@ -1,7 +1,7 @@
 package DBIx::Class::Schema::Loader::DBI::Writing;
 use strict;
 
-our $VERSION = '0.03999_02';
+our $VERSION = '0.04000';
 
 # Empty. POD only.
 
