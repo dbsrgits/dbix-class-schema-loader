1fa1884903b09903ce89a3814e41bac9e91eea9d
Rafael Kitover <rkitover@cpan.org>
2010-01-03 12:35:52 +0000

    new dev release

--- lib/DBIx/Class/Schema/Loader/Base.pm
+++ lib/DBIx/Class/Schema/Loader/Base.pm
@@ -16,7 +16,7 @@ use File::Temp qw//;
 use Class::Unload;
 require DBIx::Class;
 
-our $VERSION = '0.04999_12';
+our $VERSION = '0.04999_13';
 
 __PACKAGE__->mk_group_ro_accessors('simple', qw/
                                 schema
--- lib/DBIx/Class/Schema/Loader/DBI/Writing.pm
+++ lib/DBIx/Class/Schema/Loader/DBI/Writing.pm
@@ -1,7 +1,7 @@
 package DBIx::Class::Schema::Loader::DBI::Writing;
 use strict;
 
-our $VERSION = '0.04999_12';
+our $VERSION = '0.04999_13';
 
 # Empty. POD only.
 
