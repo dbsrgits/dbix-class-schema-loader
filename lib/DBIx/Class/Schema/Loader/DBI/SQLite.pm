dca5cd028274876a0c531c007aae5d20611b617d
Andrew Rodland <andrew@hbslabs.com>
2010-02-25 03:35:59 -0600

    Do SQLite loading without any hairy regexes.
    Well... besides the ones in DBD::SQLite anyway.
    _sqlite_parse_table is gone, and the other SQLite-specific funcs now
    use SQLite introspection stuffs. Probably requires newer SQLite vers.

--- lib/DBIx/Class/Schema/Loader/DBI/SQLite.pm
+++ lib/DBIx/Class/Schema/Loader/DBI/SQLite.pm
@@ -49,116 +49,31 @@ sub rescan {
     $self->next::method($schema);
 }
 
-# XXX this really needs a re-factor
-sub _sqlite_parse_table {
-    my ($self, $table) = @_;
-
-    my @rels;
-    my @uniqs;
-    my %auto_inc;
-
-    my $dbh = $self->schema->storage->dbh;
-    my $sth = $self->{_cache}->{sqlite_master}
-        ||= $dbh->prepare(q{SELECT sql FROM sqlite_master WHERE tbl_name = ?});
-
-    $sth->execute($table);
-    my ($sql) = $sth->fetchrow_array;
-    $sth->finish;
-
-    # Cut "CREATE TABLE ( )" blabla...
-    $sql =~ /^[\w\s"]+\((.*)\)$/si;
-    my $cols = $1;
-
-    # strip single-line comments
-    $cols =~ s/\-\-.*\n/\n/g;
-
-    # temporarily replace any commas inside parens,
-    # so we don't incorrectly split on them below
-    my $cols_no_bracketed_commas = $cols;
-    while ( my $extracted =
-        ( extract_bracketed( $cols, "()", "[^(]*" ) )[0] )
-    {
-        my $replacement = $extracted;
-        $replacement              =~ s/,/--comma--/g;
-        $replacement              =~ s/^\(//;
-        $replacement              =~ s/\)$//;
-        $cols_no_bracketed_commas =~ s/$extracted/$replacement/m;
-    }
-
-    # Split column definitions
-    for my $col ( split /,/, $cols_no_bracketed_commas ) {
-
-        # put the paren-bracketed commas back, to help
-        # find multi-col fks below
-        $col =~ s/\-\-comma\-\-/,/g;
-
-        $col =~ s/^\s*FOREIGN\s+KEY\s*//i;
-
-        # Strip punctuations around key and table names
-        $col =~ s/[\[\]'"]/ /g;
-        $col =~ s/^\s+//gs;
-
-        # Grab reference
-        chomp $col;
-
-        if($col =~ /^(.*)\s+UNIQUE/i) {
-            my $colname = $1;
-            $colname =~ s/\s+.*$//;
-            push(@uniqs, [ "${colname}_unique" => [ lc $colname ] ]);
-        }
-        elsif($col =~/^\s*UNIQUE\s*\(\s*(.*)\)/i) {
-            my $cols = $1;
-            $cols =~ s/\s+$//;
-            my @cols = map { lc } split(/\s*,\s*/, $cols);
-            my $name = join(q{_}, @cols) . '_unique';
-            push(@uniqs, [ $name => \@cols ]);
-        }
-
-        if ($col =~ /AUTOINCREMENT/i) {
-            $col =~ /^(\S+)/;
-            $auto_inc{lc $1} = 1;
-        }
-
-        next if $col !~ /^(.*\S)\s+REFERENCES\s+(\w+) (?: \s* \( (.*) \) )? /six;
-
-        my ($cols, $f_table, $f_cols) = ($1, $2, $3);
-
-        if($cols =~ /^\(/) { # Table-level
-            $cols =~ s/^\(\s*//;
-            $cols =~ s/\s*\)$//;
-        }
-        else {               # Inline
-            $cols =~ s/\s+.*$//s;
-        }
-
-        my @cols = map { s/\s*//g; lc $_ } split(/\s*,\s*/,$cols);
-        my $rcols;
-        if($f_cols) {
-            my @f_cols = map { s/\s*//g; lc $_ } split(/\s*,\s*/,$f_cols);
-            croak "Mismatched column count in rel for $table => $f_table"
-              if @cols != @f_cols;
-            $rcols = \@f_cols;
-        }
-        push(@rels, {
-            local_columns => \@cols,
-            remote_columns => $rcols,
-            remote_table => $f_table,
-        });
-    }
-
-    return { rels => \@rels, uniqs => \@uniqs, auto_inc => \%auto_inc };
-}
-
 sub _extra_column_info {
     my ($self, $table, $col_name, $sth, $col_num) = @_;
     ($table, $col_name) = @{$table}{qw/TABLE_NAME COLUMN_NAME/} if ref $table;
     my %extra_info;
 
-    $self->{_sqlite_parse_data}->{$table} ||=
-        $self->_sqlite_parse_table($table);
+    my $dbh = $self->schema->storage->dbh;
+    my $get_seq;
+    eval {
+      $get_seq = $self->{_cache}->{sqlite_sequence}
+        ||= $dbh->prepare(q{SELECT count(*) FROM sqlite_sequence WHERE name = ?});
+      $get_seq->execute($table);
+    };
+    return {} if $@;
+    my ($has_autoinc) = $get_seq->fetchrow_array;
+    $get_seq->finish;
 
-    if ($self->{_sqlite_parse_data}->{$table}->{auto_inc}->{$col_name}) {
-        $extra_info{is_auto_increment} = 1;
+    if ($has_autoinc) {
+        my $sth = $dbh->prepare(
+            "pragma table_info(" . $dbh->quote_identifier($table) . ")"
+        );
+        $sth->execute;
+        my $cols = $sth->fetchall_hashref('name');
+        if ($cols->{$col_name}{pk}) {
+            $extra_info{is_auto_increment} = 1;
+        }
     }
 
     return \%extra_info;
@@ -167,19 +82,55 @@ sub _extra_column_info {
 sub _table_fk_info {
     my ($self, $table) = @_;
 
-    $self->{_sqlite_parse_data}->{$table} ||=
-        $self->_sqlite_parse_table($table);
+    my $dbh = $self->schema->storage->dbh;
+    my $sth = $dbh->prepare(
+        "pragma foreign_key_list(" . $dbh->quote_identifier($table) . ")"
+    );
+    $sth->execute;
 
-    return $self->{_sqlite_parse_data}->{$table}->{rels};
+    my @rels;
+    while (my $fk = $sth->fetchrow_hashref) {
+        my $rel = $rels[ $fk->{id} ] ||= {
+            local_columns => [],
+            remote_columns => undef,
+            remote_table => lc $fk->{table}
+        };
+
+        push @{ $rel->{local_columns} }, lc $fk->{from};
+        push @{ $rel->{remote_columns} }, lc $fk->{to} if defined $fk->{to};
+        warn "This is supposed to be the same rel but remote_table changed from ",
+            $rel->{remote_table}, " to ", $fk->{table}
+            if $rel->{remote_table} ne lc $fk->{table};
+    }
+    $sth->finish;
+    return \@rels;
 }
 
 sub _table_uniq_info {
     my ($self, $table) = @_;
 
-    $self->{_sqlite_parse_data}->{$table} ||=
-        $self->_sqlite_parse_table($table);
+    my $dbh = $self->schema->storage->dbh;
+    my $sth = $dbh->prepare(
+        "pragma index_list(" . $dbh->quote($table) . ")"
+    );
+    $sth->execute;
 
-    return $self->{_sqlite_parse_data}->{$table}->{uniqs};
+    my @uniqs;
+    while (my $idx = $sth->fetchrow_hashref) {
+        next unless $idx->{unique};
+        my $name = $idx->{name};
+
+        my $get_idx_sth = $dbh->prepare("pragma index_info(" . $dbh->quote($name) . ")");
+        $get_idx_sth->execute;
+        my @cols;
+        while (my $idx_row = $get_idx_sth->fetchrow_hashref) {
+            push @cols, lc $idx_row->{name};
+        }
+        $get_idx_sth->finish;
+        push @uniqs, [ $name => \@cols ];
+    }
+    $sth->finish;
+    return \@uniqs;
 }
 
 sub _tables_list {
