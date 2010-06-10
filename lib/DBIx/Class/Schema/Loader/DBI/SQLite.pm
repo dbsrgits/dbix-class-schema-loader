4a1323d2bfc068aa8acec1ca3b85776fafba4783
Rafael Kitover <rkitover@cpan.org>
2010-06-10 01:44:33 -0400

    fix unique constraint names for SQLite (actual names break ->deploy)

--- lib/DBIx/Class/Schema/Loader/DBI/SQLite.pm
+++ lib/DBIx/Class/Schema/Loader/DBI/SQLite.pm
@@ -134,6 +134,7 @@ sub _table_uniq_info {
     my @uniqs;
     while (my $idx = $sth->fetchrow_hashref) {
         next unless $idx->{unique};
+
         my $name = $idx->{name};
 
         my $get_idx_sth = $dbh->prepare("pragma index_info(" . $dbh->quote($name) . ")");
@@ -143,6 +144,11 @@ sub _table_uniq_info {
             push @cols, $self->_lc($idx_row->{name});
         }
         $get_idx_sth->finish;
+
+        # Rename because SQLite complains about sqlite_ prefixes on identifiers
+        # and ignores constraint names in DDL.
+        $name = (join '_', @cols) . '_unique';
+
         push @uniqs, [ $name => \@cols ];
     }
     $sth->finish;
