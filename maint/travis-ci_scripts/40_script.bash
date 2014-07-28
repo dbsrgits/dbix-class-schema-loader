64c34f2746490f264b25f7373fad68f2dcde51ff
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2014-07-28 19:15:52 +0100

    Disable parallel testing for now

--- maint/travis-ci_scripts/40_script.bash
+++ maint/travis-ci_scripts/40_script.bash
@@ -4,7 +4,7 @@ source maint/travis-ci_scripts/common.bash
 if [[ -n "$SHORT_CIRCUIT_SMOKE" ]] ; then return ; fi
 
 run_harness_tests() {
-  local -x HARNESS_OPTIONS=c:j$NUMTHREADS
+  local -x HARNESS_OPTIONS=c
   make test 2> >(tee "$TEST_STDERR_LOG")
 }
 
@@ -14,7 +14,7 @@ if [[ "$CLEANTEST" = "true" ]] ; then
   run_or_err "Prepare blib" "make pure_all"
   run_harness_tests
 else
-  PROVECMD="prove -lrswj$NUMTHREADS xt t"
+  PROVECMD="prove -lrsw xt t"
 
   # FIXME - temporary, until Package::Stash is fixed
   if perl -M5.010 -e 1 &>/dev/null ; then
