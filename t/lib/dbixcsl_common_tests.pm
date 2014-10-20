0787c203e7e9e362f6fca95c02de1ee3817538ff
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2014-10-20 12:34:21 +0100

    Fix MySQL non-InnoDB test skip counts

--- t/lib/dbixcsl_common_tests.pm
+++ t/lib/dbixcsl_common_tests.pm
@@ -629,7 +629,7 @@ qr/\n__PACKAGE__->load_components\("TestSchemaComponent", "\+TestSchemaComponent
         'is_nullable=1 detection';
 
     SKIP: {
-        skip $self->{skip_rels}, 142 if $self->{skip_rels};
+        skip $self->{skip_rels}, 149 if $self->{skip_rels};
 
         my $moniker3 = $monikers->{loader_test3};
         my $class3   = $classes->{loader_test3};
