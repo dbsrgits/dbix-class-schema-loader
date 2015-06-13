08d89f29549b4045adb8c1d802d29109043a7a7f
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2015-06-13 14:11:53 +0100

    Add .dir-locals.el file

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil . ((indent-tabs-mode . nil)))
+ (cperl-mode . ((cperl-indent-level . 4)
+                (cperl-continued-statement-offset . 4)
+                (cperl-continued-brace-offset . 0)
+                (cperl-close-paren-offset . -4)
+                (cperl-indent-subs-specially . nil)
+                (indent-tabs-mode . nil))))
