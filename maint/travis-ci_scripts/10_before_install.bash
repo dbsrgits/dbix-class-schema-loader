8bf80223ac13a53689d007e409fed40a9c9890c5
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2017-10-10 15:27:04 +0100

    Update Firebird ODBC driver version
    Also fix illegal string concatenation error under C++11

--- maint/travis-ci_scripts/10_before_install.bash
+++ maint/travis-ci_scripts/10_before_install.bash
@@ -119,9 +119,11 @@ else
 
         run_or_err "Fetching and building Firebird ODBC driver" '
           cd "$(mktemp -d)"
-          wget -O- http://sourceforge.net/projects/firebird/files/firebird-ODBC-driver/2.0.3-Release/OdbcJdbc-src-2.0.3.154.tar.gz/download | tar -xz
+          wget -O- http://sourceforge.net/projects/firebird/files/firebird-ODBC-driver/2.0.5-Release/OdbcJdbc-src-2.0.5.156.tar.gz/download | tar -xz
           cd OdbcJdbc/Builds/Gcc.lin
           perl -p -i -e "s|/usr/lib64|/usr/lib/x86_64-linux-gnu|g" ../makefile.environ
+          # Fix illegal string concatenation error under C++11
+          perl -p -i -e "s|(\"\.\")| \$1 |g" ../../SetupAttributes.h
           make -f makefile.linux
           sudo make -f makefile.linux install
         '
